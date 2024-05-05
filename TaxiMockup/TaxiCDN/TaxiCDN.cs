42f11a303b5c182397849ff365722560012840b5
Ilija Spasic <spasicilija1@gmail.com>
2024-05-05 23:16:39 +0200

    Implement image cdn service for user pictures.

--- TaxiMockup/TaxiCDN/TaxiCDN.cs
+++ TaxiMockup/TaxiCDN/TaxiCDN.cs
@@ -36,12 +36,65 @@ namespace TaxiCDN
                                     .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
                                     .UseUrls(url);
                         var app = builder.Build();
-                        app.MapGet("/", () => "Hello World!");
-                        
+
+                        // Configure route to accept image upload
+                        app.MapPost("/cdn/upload", UploadImage);
+
+                        // Configure route to retrieve image by URL
+                        app.MapGet("/cdn/image/{fileName}", GetImage);
+
                         return app;
 
                     }))
             };
+        }
+
+        private async Task UploadImage(HttpContext context)
+        {
+            var files = context.Request.Form.Files;
+            if (files.Count == 0)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync("No file uploaded.");
+                return;
+            }
+
+            var file = files[0];
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine("wwwroot/images", fileName);
+
+            // TODO possible image resizing
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var imageUrl = $"{context.Request.Scheme}://{context.Request.Host}/cdn/image/{fileName}";
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            await context.Response.WriteAsync(imageUrl);
+        }
+
+        private async Task GetImage(HttpContext context)
+        {
+            var fileName = context.Request.RouteValues["fileName"].ToString();
+            if(string.IsNullOrWhiteSpace(fileName))
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync("Image not found.");
+                return;
+            }
+            var filePath = Path.Combine("wwwroot/images", fileName);
+
+            if (!File.Exists(filePath))
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync("Image not found.");
+                return;
+            }
+
+            context.Response.Headers.Append("Content-Type", "image/png");
+            await context.Response.SendFileAsync(filePath);
         }
     }
 }
